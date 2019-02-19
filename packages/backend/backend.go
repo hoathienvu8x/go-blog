4ceff5afde7fb445e3cde0544b244abbb152503d
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2019-02-19 16:27:44 +0700

    Add front end + back end packages

---
+++ packages/backend/backend.go
@@ -0,0 +1,21 @@
+package backend
+
+import (
+    "fmt"
+)
+
+func IsLogin() bool {
+    
+}
+
+func AuthFilter() {
+    
+}
+
+func HandleLogin() {}
+
+func HandleLoginPOST() {}
+
+func GetBackEnd() {}
+
+func HandleProfile() {}
---
+++ packages/frontend/frontend.go
@@ -0,0 +1,7 @@
+package frontend
+
+import (
+    "fmt"
+)
+
+func HandleHomePage() {}
